77a061e752f43309025314d049fc682ada7afe26
MoAfridi <afridihossain220@gmail.com>
2026-07-03 00:40:27 +0600

    Login Page Done

--- lib/repository/screens/loginscreen/loginscreen.dart
+++ lib/repository/screens/loginscreen/loginscreen.dart
@@ -19,42 +19,77 @@ class Loginscreen extends StatelessWidget {
               fontsize: 20,
               fontfamily: "bold",
             ),
-            SizedBox(height: 20,),
+            SizedBox(height: 20),
             Card(
               elevation: 4,
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10)
+                borderRadius: BorderRadius.circular(10),
               ),
               child: Container(
                 height: 200,
                 width: 350,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
-                  color: Color(0XFFFFFFF)
+                  color: Color(0XFFFFFFF),
                 ),
                 child: Column(
                   children: [
-                    SizedBox(height: 10,),
-                    Uihelper.CustomText(text: "Afridi", color: Color(0XFF000000), fontweight: FontWeight.w500, fontsize: 14),
-                    SizedBox(height: 5,),
-                    Uihelper.CustomText(text: "78277XXXX", color: Color(0XFF000000), fontweight: FontWeight.w500, fontsize: 14),
-                    SizedBox(height: 5,),
+                    SizedBox(height: 10),
+                    Uihelper.CustomText(
+                      text: "Afridi",
+                      color: Color(0XFF000000),
+                      fontweight: FontWeight.w500,
+                      fontsize: 14,
+                    ),
+                    SizedBox(height: 5),
+                    Uihelper.CustomText(
+                      text: "78277XXXX",
+                      color: Color(0XFF000000),
+                      fontweight: FontWeight.w500,
+                      fontsize: 14,
+                      fontfamily: "bold",
+                    ),
+                    SizedBox(height: 5),
                     SizedBox(
                       height: 48,
                       width: 295,
-                    child:
-                    ElevatedButton(
-                        onPressed: (){},
-                        style: ElevatedButton.styleFrom(),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFE23744),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
                         child: Row(
-                      children: [
-                        Uihelper.CustomText(text: "Afridi", color: Color(), fontweight: fontweight, fontsize: fontsize)
-                      ],
-                    )),)
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Uihelper.CustomText(
+                              text: "Login  with",
+                              color: Colors.white,
+                              fontweight: FontWeight.bold,
+                              fontsize: 14,
+                            ),
+                            SizedBox(width: 5),
+                            Uihelper.CustomImage(img: "image 9.png"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8,),
+                    Uihelper.CustomText(
+                      text:
+                          "Access your saved addresses from Zomato automatically!",
+                      color: Color(0xFF9C9C9C),
+                      fontweight: FontWeight.normal,
+                      fontsize: 10,
+                    ),
+                    SizedBox(height: 30,),
+                    Uihelper.CustomText(text: "or login with phone number", color: Color(0xFF269237), fontweight: FontWeight.w400, fontsize: 14),
                   ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
