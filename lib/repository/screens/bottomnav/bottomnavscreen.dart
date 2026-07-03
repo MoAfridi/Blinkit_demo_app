7749eb4604dcd132f7581f37502bb4f7a4e16885
MoAfridi <afridihossain220@gmail.com>
2026-07-03 17:03:16 +0600

    working

---
+++ lib/repository/screens/bottomnav/bottomnavscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ButtomNavScreen extends StatefulWidget{
+  @override
+  State<ButtomNavScreen> createState() => _ButtomNavScreenState();
+}
+
+class _ButtomNavScreenState extends State<ButtomNavScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
